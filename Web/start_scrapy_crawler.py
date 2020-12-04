966d6ed7479b5a90084ec24d11853548ca3c53d8
adam l <livneadam@gmail.com>
2020-12-04 12:10:35 +0200

    web scraper works

--- Web/start_scrapy_crawler.py
+++ Web/start_scrapy_crawler.py
@@ -2,9 +2,13 @@ import os
 
 
 def main():
-    os.system(
-        "scrapy runspider \"C:\\Users\\adam l\\Desktop\\python files\\BigData\\Web\\scrapy_web_crawler.py\" -o quotes.jl"
-    )
+    path = r"C:\Users\adam l\Desktop\python files\BigData\Web\scrapy_web_crawler.py"
+
+    os.system(f"scrapy runspider \"{path}\" -o quotes.jl")
+
+    # os.system(
+    # "scrapy runspider \"C:\\Users\\adam l\\Desktop\\python files\\BigData\\Web\\scrapy_web_crawler.py\" -o quotes.jl"
+    # )
 
 
 if __name__ == '__main__':
