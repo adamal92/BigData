94cd6c5b1a20151af3b9be72d9e2d45e7347589b
adam l <livneadam@gmail.com>
2021-01-02 19:13:02 +0200

    spider works

--- BD_projects/moto_prices/scrapy_spider.py
+++ BD_projects/moto_prices/scrapy_spider.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Any
+
 import scrapy
 
 
@@ -7,32 +9,74 @@ class MotorSpider(scrapy.Spider):
         'https://centro.co.il/en/bike/yamaha/',
     ]
 
+    # def parse(self, response, **kwargs):
+    #     # yield {'price': response.css('p.price::text').extract()}  # ::text
+    #     for price in response.css('p.price::text').extract():
+    #         print(price)
+    #         yield {
+    #             "price": price
+    #         }
+    #
+    #     print(response.css('a.name').extract())
+    #     print(response.css('a.name::text'))
+    #     # print(response.css('a.name').get().xpath('strong/text()'))
+    #
+    #     for name in response.css('a.name'):
+    #         print(name.xpath('strong/text()').get())
+    #
+    #     #
+    #     # for price in response.css('p.price'):
+    #     #     yield {
+    #     #         # 'price2': price.xpath('span/small/text()').get(),
+    #     #         # 'price': price.css('p.price > span::text').extract(),
+    #     #         'price3': price.css('span.text::text').get(),
+    #     #     }
+    #     #
+    #     # yield {'price': response.css('p.price')}
+    #
+    #     # # get next page
+    #     # next_page = response.css('li.next a::attr("href")').get()
+    #     # if next_page is not None:
+    #     #     yield response.follow(next_page, self.parse)
+
+    # def parse(self, response, **kwargs):
+    #     motorcycles = {}
+    #
+    #     for name in response.css('a.name'):
+    #         print(name.xpath('strong/text()').get())
+    #         moto_name: str = name.xpath('strong/text()').get()
+    #         motorcycles[moto_name] = {}
+    #
+    #     for price in response.css('p.price::text').extract():
+    #          print(price)
+    #          motorcycles[]
+    #
+    #      print(response.css('a.name').extract())
+    #      print(response.css('a.name::text'))
+    #
+    #
+    #      yield motorcycles
+
     def parse(self, response, **kwargs):
-        # yield {'price': response.css('p.price::text').extract()}  # ::text
-        for price in response.css('p.price::text').extract():
-            print(price)
+        print(response.css('li.mdl-list__item'))
+
+        for motorcycle in response.css('li.mdl-list__item'):
+            print(motorcycle.xpath('div/a/strong/text()').get())
+            print(motorcycle.xpath('a/p/text()').get())
+
             yield {
-                "price": price
+                "model": motorcycle.xpath('div/a/strong/text()').get(),
+                "price": motorcycle.xpath('a/p/text()').get()
             }
-
-        print(response.css('a.name').extract())
-        print(response.css('a.name::text'))
-        # print(response.css('a.name').get().xpath('strong/text()'))
-
-        for name in response.css('a.name'):
-            print(name.xpath('strong/text()').get())
-
+        # for name in response.css('a.name'):
+        #     print(name.xpath('strong/text()').get())
+        #     moto_name: str = name.xpath('strong/text()').get()
+        #     motorcycles[moto_name] = {}
         #
-        # for price in response.css('p.price'):
-        #     yield {
-        #         # 'price2': price.xpath('span/small/text()').get(),
-        #         # 'price': price.css('p.price > span::text').extract(),
-        #         'price3': price.css('span.text::text').get(),
-        #     }
+        # for price in response.css('p.price::text').extract():
+        #     print(price)
+        #     # motorcycles[]
         #
-        # yield {'price': response.css('p.price')}
+        # print(response.css('a.name').extract())
+        # print(response.css('a.name::text'))
 
-        # # get next page
-        # next_page = response.css('li.next a::attr("href")').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
--- Web/Gui/moto_prices/motorcycles.jl
+++ Web/Gui/moto_prices/motorcycles.jl
@@ -1,10 +1,10 @@
-{"price": "\u20aa10,000"}
-{"price": "\u20aa33,000"}
-{"price": "\u20aa30,000"}
-{"price": "\u20aa30,000"}
-{"price": "\u20aa9,000"}
-{"price": "\u20aa20,922"}
-{"price": "\u20aa25,000"}
-{"price": "\u20aa35,000"}
-{"price": "\u20aa19,000"}
-{"price": "\u20aa45,000"}
+{"model": "2001' Yamaha", "price": "\u20aa10,000"}
+{"model": "2020' Yamaha", "price": "\u20aa33,000"}
+{"model": "1998' Yamaha Xjr", "price": "\u20aa30,000"}
+{"model": "2016' Yamaha \u043c\u0442 07 \u0442\u0440\u0435\u0439\u0441\u0435\u0440", "price": "\u20aa30,000"}
+{"model": "2014' Yamaha 600cc", "price": "\u20aa9,000"}
+{"model": "2017' Yamaha mt 09", "price": "\u20aa20,922"}
+{"model": "2018' Yamaha Tricity 155", "price": "\u20aa25,000"}
+{"model": "2016' Yamaha \u043c\u0442 07 \u0442\u0440\u0435\u0439\u0441\u0435\u0440", "price": "\u20aa35,000"}
+{"model": "2007' Yamaha \u05d9\u05de\u05d0\u05d4\u05d4 \u05e4\u05d9\u05d9\u05d6\u05e8 600", "price": "\u20aa19,000"}
+{"model": "2016' Yamaha \u05d9\u05de\u05d0\u05d4\u05d4 MT 07", "price": "\u20aa45,000"}
