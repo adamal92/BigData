6d1c74fcc5e99ae16e432738619cfacc19fbe944
adam l <livneadam@gmail.com>
2021-01-05 16:35:28 +0200

    filter ver 1

--- BD_projects/moto_prices/dirty_spider_2.py
+++ BD_projects/moto_prices/dirty_spider_2.py
@@ -1,16 +1,19 @@
 import re
 
+import bs4
 import scrapy
 import logging
+
+from bs4 import BeautifulSoup
 
 
 class DirtyMotoSpider(scrapy.Spider):
     name = 'motors'
     start_urls = [
-        # 'https://centro.co.il/en/bike/yamaha/'
-        # ,
-        # 'https://centro.co.il/en/auto/'
-        # ,
+        'https://centro.co.il/en/bike/yamaha/'
+        ,
+        'https://centro.co.il/en/auto/'
+        ,
         'https://www.kawasaki.co.il/motorbikes/supernaked/z900_35kw/?_'
         'ga=2.193938925.1786289935.1609773572-1769228414.1609773572'
     ]
@@ -21,7 +24,7 @@ class DirtyMotoSpider(scrapy.Spider):
             logging.error("failed to access website")
 
         from bs4 import BeautifulSoup
-        soup = BeautifulSoup(str(response.css('body').getall().pop()), 'html.parser')
+        soup: BeautifulSoup = BeautifulSoup(str(response.css('body').getall().pop()), 'html.parser')
         # print(str(response.css('body').getall()))
         # print(soup.prettify())
         # for child in soup.body.children:
@@ -29,7 +32,13 @@ class DirtyMotoSpider(scrapy.Spider):
         #     # # print(type(child))
         #     # print("\n---------child-----------------------------------------\n")
         #     DirtyMotoSpider.recurse_over_html_tree(soup_Tag=child)
-        DirtyMotoSpider.recurse_over_html_tree(soup_Tag=soup.body)
+        # DirtyMotoSpider.recurse_over_html_tree(soup_Tag=soup.body)
+
+        # for tag in soup.find_all(lambda tag: tag.stripped_strings):
+        for tag in soup.find_all(DirtyMotoSpider.filter_unstripped_strings):
+            print("\n---------child-----------------------------------------\n")
+            for string in tag.stripped_strings:
+                print(string)
 
         next_page = response.css('div.next a::attr("href")').get()
         if next_page is not None:
@@ -40,15 +49,36 @@ class DirtyMotoSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     @staticmethod
-    def recurse_over_html_tree(soup_Tag):
+    def recurse_over_html_tree(soup_Tag: bs4.element.Tag):
         # if soup_Tag.children is None:
         #     print("\n---------child-----------------------------------------\n")
         #     print(soup_Tag.prettify())
         #     return
         try:
+            # print(type(soup_Tag), type(soup_Tag.children))
             for child in soup_Tag.children:
                 DirtyMotoSpider.recurse_over_html_tree(child)
-        except:
-            print("\n---------child-----------------------------------------\n")
+        except:  # if child is a leaf
             # print(soup_Tag.prettify())
-            print(soup_Tag.string)
+            # print(soup_Tag.string)
+            # if soup_Tag.string.isalnum() and soup_Tag.string.strip():
+            #     print("\n---------child-----------------------------------------\n")
+            #     print(soup_Tag.string)  # .strip("\n\t\r\b\"@:?{}[]<>;.")
+            # if text.strip(): print(text)  # .strip("\n\t\r\b\"@:?{}[]<>;.")
+            # if re.search('[^A-Za-z0-9]', string=soup_Tag.string) and soup_Tag.string.strip():
+            #     print("\n---------child-----------------------------------------\n")
+            #     print(soup_Tag.string)  # .strip("\n\t\r\b\"@:?{}[]<>;.")
+            for string in soup_Tag.stripped_strings:
+                print("\n---------child-----------------------------------------\n")
+                print(repr(string))
+            print(soup_Tag.parent)
+
+    @staticmethod
+    def filter_unstripped_strings(tag: bs4.element.Tag) -> bool:
+        has_special_chars: bool = False
+
+        for string in tag.stripped_strings:
+            if string is None and not string.strip():
+                has_special_chars = True
+
+        return not has_special_chars
